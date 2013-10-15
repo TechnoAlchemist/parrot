2636bc27ca331df0bc673c84108b9d8a4261b167
Gabriel Lafontant <glafonta@gmail.com>
2013-10-15 19:59:18 -0400

    passing tests for create cohorts feature

--- spec/features/admin_creates_cohort_spec.rb
+++ spec/features/admin_creates_cohort_spec.rb
@@ -12,12 +12,12 @@ feature "admin_creates_a_cohort", %Q{
 # * If the admin attempts to create a cohort without entering required information,
 #   they will receive an error message
 
-  let(:user) {FactoryGirl.create(:user)}
   let(:cohort) {FactoryGirl.build(:cohort)}
 
   scenario "admin successfully creates cohort" do
     prev_count = Cohort.count
-    # sign_in_as(user)
+    set_omniauth
+    sign_in
     visit new_cohort_path
     fill_in "Term", with: cohort.term
     click_button "Create New Cohort"
@@ -28,7 +28,8 @@ feature "admin_creates_a_cohort", %Q{
 
   scenario "admin fails to create cohort" do
     prev_count = Cohort.count
-    # sign_in_as(user)
+    set_omniauth
+    sign_in
     visit new_cohort_path
     click_button "Create New Cohort"
 
@@ -39,10 +40,9 @@ feature "admin_creates_a_cohort", %Q{
 
   context "as an unauthenticated user" do
     scenario "when user visits new cohort page and can not create cohort" do
-      pending visit new_cohort_path
+      visit new_cohort_path
 
       expect(page).to have_content "You must be signed in to view this page"
-      expect(page).to_not have_content "Term"
     end
   end
 end
