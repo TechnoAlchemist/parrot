2809c9d75ba1ea0ec692135c8d38eb21d8e17f2a
Tom Corley <tomcorleycarpentry@gmail.com>
2013-10-16 09:45:47 -0400

    migrate now works

--- db/migrate/20131015212209_add_null_false_to_users_ui_dand_provider.rb
+++ db/migrate/20131015212209_add_null_false_to_users_ui_dand_provider.rb
@@ -1,6 +1,11 @@
 class AddNullFalseToUsersUiDandProvider < ActiveRecord::Migration
-  def change
+  def up
     change_column :users, :provider, :string, null: false
     change_column :users, :uid, :string, null: false
+  end
+
+  def down
+    change_column :users, :provider, :string
+    change_column :users, :uid, :string
   end
 end
